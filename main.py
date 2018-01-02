587096233a5861344d1cdf6b28eb5001f4ed5490
pipluppers <anguy086@ucr.edu>
2018-01-02 00:30:32 +0000

    c

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+
+def loadData(fileName):
+	return pd.read_csv(fileName)
+
+
+def main():
+#	print("Hello World")
+#	print(2)
+
+main()
